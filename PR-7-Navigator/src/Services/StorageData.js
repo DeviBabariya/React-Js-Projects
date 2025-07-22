3af4bf4631f4b7c288f511dbe2608f26ddcc94ec
Devi Babariya <devibabariyarnw@gmail.com>
2025-07-22 14:16:04 +0530

    Delete PR-7-Navigator directory

--- PR-7-Navigator/src/Services/StorageData.js
+++
@@ -1,7 +0,0 @@
-export const getStorageData = () => {
-    return JSON.parse(localStorage.getItem("products")) || [];
-}
-
-export const setStorageData = (data) => {
-    localStorage.setItem("products", JSON.stringify(data));
-}
